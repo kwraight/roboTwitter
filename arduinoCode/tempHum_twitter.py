04497d6222faf02ccd968ac46042aada130ed89b
kwraight <kenneth.gibb.wraight@gmail.com>
2018-08-28 13:55:00 +0000

    python tempHum code

---
+++ arduinoCode/tempHum_twitter.py
@@ -0,0 +1,46 @@
+""" 
+dht22.py 
+Temperature/Humidity monitor using Raspberry Pi and DHT22. 
+Data is displayed at thingspeak.com
+Original author: Mahesh Venkitachalam at electronut.in 
+Modified by Adam Garbo on December 1, 2016 
+""" 
+import sys 
+import RPi.GPIO as GPIO
+from datetime import datetime
+from time import sleep 
+import Adafruit_DHT 
+import urllib2
+sys.path.insert(0, '/home/pi/repositories/configs/')
+import configSettings_ppe as configSettings
+
+GPIO_PIN=19
+
+
+def tweetInfo(infotxt, type, id):
+    api=configSettings.get_api()
+    tweet = str(infotxt)+" #"+str(type)+" #"+str(id)+" at: "+str(datetime.now()) # add time to avoid repeated tweets
+    info = api.update_status(status=tweet)
+    # Yes, tweet is called 'status' rather confusing
+    return info
+
+def getSensorData(): 
+   RH, T = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, GPIO_PIN) 
+   return (str(RH), str(T)) 
+def main(): 
+   print 'starting...' 
+   while True:
+       if 1==1:
+       #try: 
+           RH, T = getSensorData() 
+           infotxt= "test from afar T/H(C/%): {0} / {1}".format(T,RH)
+           print infotxt
+           tweetInfo(infotxt,"tempHum","RoboPi")
+           sleep(300) #loads DHT22 sensor values every 5 minutes
+       #except: 
+           print 'exiting.' 
+           break 
+# call main 
+if __name__ == '__main__': 
+   main()
+   
--- postInfo.py
+++ postInfo.py
@@ -1,39 +1,13 @@
 import tweepy
 from datetime import datetime, date, timedelta
 import sys
-import argumentClass
-sys.path.insert(0, '../configs/')
+sys.path.insert(0, '/home/pi/repositories/configs/')
 import configSettings_ppe as configSettings
 
 def postInfo(val, type, id):
     api=configSettings.get_api()
-    for i in range(0,5,1):
-        tweet = "the value is: "+str(val)+" #"+str(type)+" #"+str(id)+" at:"+str(datetime.now()) # add time to avoid repeated tweets
-        status = api.update_status(status=tweet)
-# Yes, tweet is called 'status' rather confusing
+    tweet = "the value is: "+str(val)+" #"+str(type)+" #"+str(id)+" at:"+str(datetime.now()) # add time to avoid repeated tweets
+    info = api.update_status(status=tweet)
+    # Yes, tweet is called 'status' rather confusing
+    return info
 
-
-def main():
-
-    print ">>>postInfo running..."
-
-    ### get the inputs
-    args = argumentClass.PostArgs()
-    #print args
-
-    postDict={}
-    ### set parameters
-    for k in vars(args).iteritems():
-        postDict[k[0]]=k[1]
-        
-    print postDict
-
-    postInfo(postDict['value'], postDict['type'], postDict['id'])
-
-    print ">>>postInfo running..."
-
-
-if __name__ == "__main__":
-    main()
-
-    #some values to post
